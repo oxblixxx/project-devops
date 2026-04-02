efc657edcaf08ffcc6eb7151e0af06132a6738dd
oXbLiXxX <97739416+oxblixxx@users.noreply.github.com>
2026-04-02 13:55:16 +0100

    Create check_db_size_in_mb.sh

---
+++ MYSQL/check_db_size_in_mb.sh
@@ -0,0 +1,6 @@
+SELECT 
+    table_schema AS "Database",
+    ROUND(SUM(data_length + index_length) / 1024 / 1024, 2) AS "Size (MB)"
+FROM information_schema.tables
+GROUP BY table_schema
+ORDER BY SUM(data_length + index_length) DESC;
